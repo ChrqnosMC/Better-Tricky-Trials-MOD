ebc71a4b015745dc47d7e4baacfeeb6579667d8b
Chrqnos <chrqnospro@gmail.com>
2025-12-07 18:21:49 +0100

    Port to 1.21.1

---
+++ src/main/java/net/chrqnos/better_tricky_trials/effect/BastionOmenEffect.java
@@ -0,0 +1,8 @@
+package net.chrqnos.better_tricky_trials.effect;
+
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+
+public class BastionOmenEffect extends StatusEffect {
+    public BastionOmenEffect(StatusEffectCategory category, int color) { super(category, color); }
+}
