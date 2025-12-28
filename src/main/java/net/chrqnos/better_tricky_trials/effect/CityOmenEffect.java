58cc53011023d227147436a47b16e6be00f8a7b2
Chrqnos <96986045+ChrqnosMC@users.noreply.github.com>
2025-12-28 02:09:14 +0100

    Remove unused files

---
+++ src/main/java/net/chrqnos/better_tricky_trials/effect/CityOmenEffect.java
@@ -0,0 +1,8 @@
+package net.chrqnos.better_tricky_trials.effect;
+
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+
+public class CityOmenEffect extends StatusEffect {
+    public CityOmenEffect(StatusEffectCategory category, int color) { super(category, color); }
+}
